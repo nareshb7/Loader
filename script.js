612282281444ac9e766d143869ca62465fb2657f
nareshb7 <nareshbjava7@gmail.com>
2023-01-17 23:11:39 +0530

    Percentage Loader Added

--- script.js
+++ script.js
@@ -9,3 +9,23 @@ setTimeout(()=>{
 content.style.display = 'block'
 loader.style.display = 'none'
 }, 8000)
+
+const loadText = document.querySelector('.blur-text')
+const bg = document.querySelector('.bg')
+
+let int = setInterval(blurring, 75)
+let load =0;
+ function blurring(){
+    load++
+
+    if (load >99){
+        clearInterval(int)
+    }
+    loadText.innerText = `${load}%`;
+    loadText.style.opacity = scale(load , 0, 100, 1,0)
+    bg.style.filter = `blur(${scale(load, 0, 100, 25, 0)}px)`
+ }
+
+ function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
